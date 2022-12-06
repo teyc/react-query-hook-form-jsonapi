c70f7a75033d981c5f86ead50d5e613672648f84
Chui Tey <chui.tey@loanmarketgroup.com>
2022-12-06 15:14:19 +1000

    Seed database with one contact

--- backend/DataAccess/MyCrmDbContext.cs
+++ backend/DataAccess/MyCrmDbContext.cs
@@ -1,40 +1,59 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SampleWeb.JsonApiResource;
 
 namespace SampleWeb.DataAccess;
 
-public class MyCrmDbContext: DbContext
+public class MyCrmDbContext : DbContext
 {
-   public MyCrmDbContext(DbContextOptions<MyCrmDbContext> options): base(options)
-   { }
-   
-   protected override void OnModelCreating(ModelBuilder builder)
-   {
-      base.OnModelCreating(builder);
+    public MyCrmDbContext(DbContextOptions<MyCrmDbContext> options) : base(options)
+    { }
 
-      if (Database.ProviderName == "Microsoft.EntityFrameworkCore.Sqlite")
-      {
-         // SQLite does not have proper support for DateTimeOffset via Entity Framework Core, see the limitations
-         // here: https://docs.microsoft.com/en-us/ef/core/providers/sqlite/limitations#query-limitations
-         // To work around this, when the Sqlite database provider is used, all model properties of type DateTimeOffset
-         // use the DateTimeOffsetToBinaryConverter
-         // Based on: https://github.com/aspnet/EntityFrameworkCore/issues/10784#issuecomment-415769754
-         // This only supports millisecond precision, but should be sufficient for most use cases.
-         foreach (var entityType in builder.Model.GetEntityTypes())
-         {
-            var properties = entityType.ClrType.GetProperties().Where(p => p.PropertyType == typeof(DateTimeOffset)
-                                                                           || p.PropertyType == typeof(DateTimeOffset?));
-            foreach (var property in properties)
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+
+        if (Database.ProviderName == "Microsoft.EntityFrameworkCore.Sqlite")
+        {
+            // SQLite does not have proper support for DateTimeOffset via Entity Framework Core, see the limitations
+            // here: https://docs.microsoft.com/en-us/ef/core/providers/sqlite/limitations#query-limitations
+            // To work around this, when the Sqlite database provider is used, all model properties of type DateTimeOffset
+            // use the DateTimeOffsetToBinaryConverter
+            // Based on: https://github.com/aspnet/EntityFrameworkCore/issues/10784#issuecomment-415769754
+            // This only supports millisecond precision, but should be sufficient for most use cases.
+            foreach (var entityType in builder.Model.GetEntityTypes())
             {
-               builder
-                  .Entity(entityType.Name)
-                  .Property(property.Name)
-                  .HasConversion(new DateTimeOffsetToStringConverter());
+                var properties = entityType.ClrType.GetProperties().Where(p => p.PropertyType == typeof(DateTimeOffset)
+                                                                               || p.PropertyType == typeof(DateTimeOffset?));
+                foreach (var property in properties)
+                {
+                    builder
+                       .Entity(entityType.Name)
+                       .Property(property.Name)
+                       .HasConversion(new DateTimeOffsetToStringConverter());
+                }
             }
-         }
-      }
-   }
-   
-   public virtual DbSet<ContactDto> Contacts { get; set; } = default!;
+        }
+
+        SeedData(builder);
+    }
+
+    public virtual DbSet<ContactDto> Contacts { get; set; } = default!;
+
+    private void SeedData(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<ContactDto>().HasData(
+           new ContactDto
+           {
+               Id = 1,
+               CreatedDate = DateTime.Now,
+               CreatedUserId = "1",
+               DateOfBirth = new DateOnly(1985, 03, 05),
+               FirstName = "Max",
+               LastName = "Melwell",
+               IsActive = 1,
+               NextOnlineMeeting = new DateTimeOffset(2022, 12, 25, 13, 0, 0, TimeSpan.FromHours(10.0))
+           }
+        );
+
+    }
 }
---
+++ backend/Migrations/20221206051146_ContactSeeding.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SampleWeb.Migrations
+{
+    /// <inheritdoc />
+    public partial class ContactSeeding : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Contact",
+                columns: new[] { "ContactId", "CreatedDate", "CreatedUserId", "DateOfBirth", "FirstName", "IsActive", "LastModifiedDate", "LastName", "ModifiedUserId", "NextOnlineMeeting" },
+                values: new object[] { 1, "2022-12-06T15:11:46.2593310+10:00", "1", new DateOnly(1985, 3, 5), "Max", 1, null, "Melwell", null, "2022-12-25T13:00:00.0000000+10:00" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Contact",
+                keyColumn: "ContactId",
+                keyValue: 1);
+        }
+    }
+}
